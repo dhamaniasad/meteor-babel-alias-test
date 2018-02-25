cef1d7c3c252f7142022a9c8b838888d0f841dc4
Asad Dhamani <dhamaniasad@gmail.com>
2018-02-25 23:53:42 +0530

    Fix demo

--- client/main.js
+++ client/main.js
@@ -1,13 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import { React } from "react";
+import React from "react";
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  window.React = React;
 });
 
 Template.hello.helpers({
